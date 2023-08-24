daea11f60054d50f73b9408c2cec8890efe7241c
hieu-mai <hieu-mai@cybozu.vn>
2023-08-24 11:07:13 +0700

    challenge 2 mock module

--- src/challenge-02-mock-module/app.test.js
+++ src/challenge-02-mock-module/app.test.js
@@ -1,5 +1,22 @@
-describe('getBestPlayer', () => {
-  it('should return the best player', () => {
+import { getBestPlayer } from "./app";
+import * as serviceModule from "./service";
+
+jest.mock("./service");
+
+describe("getBestPlayer", () => {
+  it("should return the best player", () => {
     // TODO: implement test
+    const users = [
+      { name: "Player 1", score: 100 },
+      { name: "Player 2", score: 200 },
+      { name: "Player 3", score: 300 },
+    ];
+    const expectUser = { name: "Player 3", score: 300 };
+    serviceModule.getAllPlayers.mockReturnValue(users);
+
+    const user = getBestPlayer();
+
+    expect(serviceModule.getAllPlayers).toHaveBeenCalled();
+    expect(user).toEqual(expectUser);
   });
 });
